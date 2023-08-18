e628e21c711770e08c97ad4f6cb490d46e40cb7a
Boris Mirsky <boris.y.mirsky@gmail.com>
2023-08-18 16:13:51 +0300

    3 files are removed

--- app/components/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-//import ReactDOM from 'react-dom';
-//import { createRoot } from 'react-dom/client';
-import {App} from './app.jsx';
-
-
-//ReactDOM.render(<App1 />, document.getElementById('root'))
-
