2045f707f221bde81bbcc74e296110865504666b
Boris Mirsky <boris.y.mirsky@gmail.com>
2023-08-18 16:21:34 +0300

    all data are going in and from localStorage only, not props

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,27 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom';
-import {TaskTable} from "./taskTable.jsx";
+//import {TaskTable} from "./taskTable.jsx";
 import {AddTaskForm} from "./taskForm.jsx";
+//import {UseLocalStorage} from "./useLocalStorage.jsx";
+import {ReturnLocalStorage} from "./fromLocalStorage.jsx";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 //import { Button } from 'react-bootstrap';
+//import css from 'main.css';
+//TaskTable tasks={tasks} deleteTask={deleteTask}/ 
 
-
-  
+ 
 const App = () => {
 	
-   const [tasks, setTasks] = useState([]); // sessionStorage)
+
+   // const [tasks, setTasks] = useState([]); // useLocalStorage([])
    
-   const addTask = task => {
-      task.id = tasks.length + 1;
-	  let task1 = Object.assign({}, task);
-      setTasks([ ...tasks, task1 ]);
-  }
+   // const addTask = task => {
+      // task.id = tasks.length + 1;
+	  // let task1 = Object.assign({}, task);
+      // setTasks([ ...tasks, task1 ]);
+  // }
   
-  const deleteTask = id => {
-    setTasks(tasks.filter(task => task.id !== id))
-  }
+  // const deleteTask = id => {
+    // setTasks(tasks.filter(task => task.id !== id))
+  // }
 
-
+ // <AddTaskForm addTask={addTask} />
    return(
       <div>
 		   <div className="container">
@@ -30,19 +34,20 @@ const App = () => {
 				   }}
 				>CRUD App with hooks & localStorage </h3>
 				   <div className="flex-row">
+				   
 					   <div 
 						   className="flex-large" 
 						   style={{backgroundColor: `#dcdcdc`}}
 					   >
 						   <h4 style={{marginTop:50,marginLeft:50}}>Add task to localStorage</h4>
-						   <AddTaskForm addTask={addTask} />
+						   <AddTaskForm />
 					   </div>
-					   <div 
-						   className="flex-large"
+					   					   
+					   <div
+					   className="flex-large"
 						   style={{backgroundColor: `#f0ffff`}}
-					   >
-						   <h4 style={{marginTop:50,marginLeft:50}}>View all tasks</h4>
-						   <TaskTable tasks={tasks} deleteTask={deleteTask}/>
+						   ><h4 style={{marginTop:50,marginLeft:50}}>View & edit all tasks</h4>
+					       <ReturnLocalStorage />
 					   </div>
 			   </div>
 		   </div>
@@ -52,4 +57,5 @@ const App = () => {
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//export { myCounter };
+
+
--- app/components/taskForm.jsx
+++ app/components/taskForm.jsx
@@ -1,88 +1,96 @@
-import React, { useState, useEffect, useId } from 'react'
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from "react-dom";
-import {useLocalStorage} from "./useLocalStorage.jsx";
-  
 
-//const AddTaskForm = () => { 
-const AddTaskForm = props => {
-  const initialFormState = { id: null, taskId: '', title: ''} ;
-  const [task, setTask] = useState(initialFormState);
-  //const [task1, setTaskLocalStorage] = useLocalStorage(key,'');
+
+
+
+const AddTaskForm = () => {
+    const initialFormState = { id: null, taskId: '', title: ''} ;
+	
+    const [task, setTask] = useState(initialFormState);
+    //const [task1, setTaskLocalStorage] = UseLocalStorage(key,'');
   
   
-  //------------counter-----------------------------
-  // не убивается во время localStorage.clear()
-  const [count, setCount] = useState(
+    //------------counter-----------------------------
+    const [count, setCount] = useState(
     Number(localStorage.getItem('counter')) || 0
-  )
-  const handleCounter = () => {
-    setCount(count + 1)
-  }
-  useEffect(()=>{
-    localStorage.setItem('counter', count)
-  }, [count])
-  //--------------------------------------------------
-  
-  
-  const handleInputChange = event => {
-    const { name, value } = event.currentTarget;
-    setTask({ ...task, [name]: value });
-  }
+    ) 	
+	const handleCounter = () => {
+		setCount(count + 1)
+	  } 
+	  useEffect(()=>{
+		localStorage.setItem('counter', count)
+	}, [count])
 
-  const handleSubmit = event => {
-	event.preventDefault();
-	localStorage.setItem(task.taskId, JSON.stringify(task.title));
-    //if (task.taskId & task.title) return
-    props.addTask(task);
-    //setTask(initialFormState);
-	handleCounter();
-	task.taskId = "";
-	task.title = "";
-  }
+  
+
+	const [_, forceUpdate] = useReducer((x) => x + 1, 0); 
+    const reRender = () => {
+        forceUpdate();
+    };
+	
+	
+    const handleInputChange = event => {
+		const { name, value } = event.currentTarget;
+		setTask({ ...task, [name]: value });
+    }  
 
 
-  async function getData() {
-	let myCounter = Number(localStorage['counter']); // +1;
- 	handleCounter();
-	let stringUrl = 'https://jsonplaceholder.typicode.com/todos/' + myCounter;
-    const response = await fetch(stringUrl, {});
-    const json = await response.json();
-	localStorage.setItem(json.id, JSON.stringify(json.title));
-  }
+     function handleSubmit(event) {
+		 event.preventDefault();
+		 localStorage.setItem(task.taskId, JSON.stringify(task.title));
+		 reRender();
+		 handleCounter();
+		 task.taskId = "";
+		 task.title = ""; 
+     }
+
+
+    async function getData() {
+		handleCounter();
+		let myCounter;
+		myCounter = localStorage['counter'] ? Number(localStorage['counter']) : 1;
+		let stringUrl = 'https://jsonplaceholder.typicode.com/todos/' + myCounter;
+		const response = await fetch(stringUrl, {});
+		const json = await response.json();
+		localStorage.setItem(json.id, JSON.stringify(json.title));
+        reRender();		  
+    }  
   
   
-  
-  return (
-      <div>
-    <form  style={{marginTop:10,marginBottom:10,marginRight:10,marginLeft:50}}>
-	     <label>Send request & get response&nbsp;&nbsp;&nbsp;</label>
-	     <button type="button" onClick={() => getData()} >
-		 get data
-		 </button>	
-	</form>		  
-    <form 
-	     onSubmit={handleSubmit}
-	     style={{marginTop:30,marginBottom:30,marginLeft:30}}>
-      <label>taskId&nbsp;&nbsp;</label>
-      <input 
-		  type="text" 
-		  name="taskId" 
-		  value={task.taskId}
-		  onChange={handleInputChange}
-		  required
-	  />
-	 <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;title&nbsp;&nbsp;</label>
-      <input 
-	     type="text"
-	     name="title" 
-		 value={task.title}
-		 onChange={handleInputChange}
-		 required
-		 />
-      <button style={{marginLeft:30,marginBottom:30}}>Add new task</button>
-    </form>
-	</div>
-  )
+	  return (
+		  <div>
+			<form  style={{marginTop:10,marginBottom:10,marginRight:10,marginLeft:50}}>
+				 <label>Add new task from fetch&nbsp;&nbsp;&nbsp;</label>
+				 <button type="button" onClick={() => { getData();}} >
+				 go
+				 </button>	
+			</form>		
+		
+			<form 
+				 onSubmit={handleSubmit}
+				 style={{marginTop:30,marginBottom:30,marginLeft:30}}>
+			  <label>taskId&nbsp;&nbsp;</label>
+			  <input 
+				  type="text" 
+				  name="taskId" 
+				  value={task.taskId}
+				  onChange={handleInputChange}
+				  required
+			  />
+			 <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;title&nbsp;&nbsp;</label>
+			  <input 
+				 type="text"
+				 name="title" 
+				 value={task.title}
+				 onChange={handleInputChange}
+				 required
+				 />
+				 <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Add new task from form</label>
+			  <button style={{marginLeft:30,marginBottom:30}}>go</button>
+			</form>
+		</div>
+	  )
 }
 
 
