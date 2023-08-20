e5426148cedaccc7ba4d7bd723544bb71aa29f67
Boris Mirsky <boris.y.mirsky@gmail.com>
2023-08-20 11:01:44 +0300

    small improvements for the better

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,53 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom';
-//import {TaskTable} from "./taskTable.jsx";
 import {AddTaskForm} from "./taskForm.jsx";
-//import {UseLocalStorage} from "./useLocalStorage.jsx";
 import {ReturnLocalStorage} from "./fromLocalStorage.jsx";
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import { Button } from 'react-bootstrap';
-//import css from 'main.css';
-//TaskTable tasks={tasks} deleteTask={deleteTask}/ 
 
  
-const App = () => {
-	
+const App = () => {	
 
-   // const [tasks, setTasks] = useState([]); // useLocalStorage([])
-   
-   // const addTask = task => {
-      // task.id = tasks.length + 1;
-	  // let task1 = Object.assign({}, task);
-      // setTasks([ ...tasks, task1 ]);
-  // }
-  
-  // const deleteTask = id => {
-    // setTasks(tasks.filter(task => task.id !== id))
-  // }
-
- // <AddTaskForm addTask={addTask} />
    return(
       <div>
 		   <div className="container">
-			   <h3 style={{
-				   backgroundColor: `#f0ffff`
-				   }}
-				>CRUD App with hooks & localStorage </h3>
+			   <h3 >CRUD операции над данными из localStorage </h3>
 				   <div className="flex-row">
 				   
 					   <div 
-						   className="flex-large" 
-						   style={{backgroundColor: `#dcdcdc`}}
-					   >
-						   <h4 style={{marginTop:50,marginLeft:50}}>Add task to localStorage</h4>
+						   className="flex-large" >
+						   <h4></h4>
 						   <AddTaskForm />
 					   </div>
 					   					   
 					   <div
-					   className="flex-large"
-						   style={{backgroundColor: `#f0ffff`}}
-						   ><h4 style={{marginTop:50,marginLeft:50}}>View & edit all tasks</h4>
-					       <ReturnLocalStorage />
+					   className="flex-large"  >
+
 					   </div>
 			   </div>
 		   </div>
@@ -55,7 +28,10 @@ const App = () => {
    )
 }
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
+export { App }
 
-
+						   //<h4 style={{marginTop:50,marginLeft:50}}>View & edit all tasks</h4>
+					       // <ReturnLocalStorage />
--- app/components/taskForm.jsx
+++ app/components/taskForm.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from "react-dom";
-
-
-
-
-const AddTaskForm = () => {
+import {Table, Input, Button,
+ InputGroup, InputGroupText, Form, 
+ FormGroup, Label, InputGroupAddon,
+ ListGroup, ListGroupItem} from 'reactstrap';
+		 
+		 
+ 
+const AddTaskForm = () => {  
     const initialFormState = { id: null, taskId: '', title: ''} ;
 	
     const [task, setTask] = useState(initialFormState);
-    //const [task1, setTaskLocalStorage] = UseLocalStorage(key,'');
-  
-  
+	
     //------------counter-----------------------------
     const [count, setCount] = useState(
     Number(localStorage.getItem('counter')) || 0
@@ -21,78 +22,182 @@ const AddTaskForm = () => {
 	  useEffect(()=>{
 		localStorage.setItem('counter', count)
 	}, [count])
-
+    //-------------------------------------------------------
   
-
-	const [_, forceUpdate] = useReducer((x) => x + 1, 0); 
-    const reRender = () => {
-        forceUpdate();
-    };
-	
-	
     const handleInputChange = event => {
 		const { name, value } = event.currentTarget;
 		setTask({ ...task, [name]: value });
     }  
-
-
+	
      function handleSubmit(event) {
 		 event.preventDefault();
 		 localStorage.setItem(task.taskId, JSON.stringify(task.title));
-		 reRender();
 		 handleCounter();
 		 task.taskId = "";
 		 task.title = ""; 
      }
-
-
+	
     async function getData() {
-		handleCounter();
 		let myCounter;
 		myCounter = localStorage['counter'] ? Number(localStorage['counter']) : 1;
 		let stringUrl = 'https://jsonplaceholder.typicode.com/todos/' + myCounter;
 		const response = await fetch(stringUrl, {});
 		const json = await response.json();
 		localStorage.setItem(json.id, JSON.stringify(json.title));
-        reRender();		  
+        handleCounter();
     }  
+
+  	// -------------- localStorage----------------------
+	const allStorage = () => {
+		    var archive = [],
+				keys = Object.keys(localStorage),
+				i = 0, key;
+			for (; key = keys[i]; i++) {
+				archive.push( key + ':' + localStorage.getItem(key) + '     ');
+			}
+			return archive;
+			}		
+			
+	const myLocalStorage = allStorage();
+	
+	
+	// ---------------force rerender--------------------
+	const [_, forceUpdate] = useReducer((x) => x + 1, 0); 	
+    const reRender = () => {
+        forceUpdate();
+    };
+			
+			
+   // ----------------delete item  -------------------------------
+   const handleDeleteTask = (id, id1) => {
+		let answer = window.confirm('Are you sure?')
+		if (answer) {
+		  localStorage.removeItem(id1);
+          reRender(); 	  
+		}
+   }; 
+
+
+	// ---------------- for checkbox -------------------------
+	 // const [table, setTable] = useState(() => myLocalStorage.map(item => {
+		  // item.checked = false;
+		  // return item;
+	 // }));	 	  
+     // const handleOnChange = (position)  => {
+	     // const tab = table.map((it) => {              
+           // if (it.id === position) {
+             // it.checked = !it.checked;
+         // }  
+         // return it;
+       // });
+	   // setTable(tab);
+    // };   
   
   
 	  return (
-		  <div>
-			<form  style={{marginTop:10,marginBottom:10,marginRight:10,marginLeft:50}}>
-				 <label>Add new task from fetch&nbsp;&nbsp;&nbsp;</label>
-				 <button type="button" onClick={() => { getData();}} >
-				 go
-				 </button>	
-			</form>		
-		
-			<form 
-				 onSubmit={handleSubmit}
-				 style={{marginTop:30,marginBottom:30,marginLeft:30}}>
-			  <label>taskId&nbsp;&nbsp;</label>
-			  <input 
-				  type="text" 
-				  name="taskId" 
-				  value={task.taskId}
-				  onChange={handleInputChange}
-				  required
-			  />
-			 <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;title&nbsp;&nbsp;</label>
-			  <input 
-				 type="text"
-				 name="title" 
-				 value={task.title}
-				 onChange={handleInputChange}
-				 required
-				 />
-				 <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Add new task from form</label>
-			  <button style={{marginLeft:30,marginBottom:30}}>go</button>
-			</form>
+	<div>
+	 <div>	
+	  <ListGroup>
+        <ListGroupItem></ListGroupItem>
+        <ListGroupItem>
+		     <Label>Задача из удалённого источника&nbsp;&nbsp;&nbsp;&nbsp;</Label>
+		     <Button 
+				 outline 
+				 color="primary" 
+				 size="sm" 
+				 onClick={() => { getData(); }} 
+			 >
+				 Добавить
+			 </Button>
+		</ListGroupItem>
+        <ListGroupItem>			
+		   <Form onSubmit={handleSubmit}>			
+				<div className="form-row">
+			     <div className="form-group col-md-3">			  
+			      <Label >TaskId</Label>
+				  <Input 
+					  className="form-control" 
+					  type="text" 
+					  name="taskId" 
+					  value={task.taskId}
+					  onChange={handleInputChange}
+					  required
+				  />			  
+				 </div>			
+				 <div className="form-group col-md-3">
+				  <Label >Title</Label>
+				   <Input 
+					 className="form-control" 
+					 type="text"
+					 name="title" 
+					 value={task.title}
+					 onChange={handleInputChange}
+					 required
+				   />
+				 </div>
+		        </div>
+		  	  <Label>Задача из формы&nbsp;&nbsp;&nbsp;&nbsp;</Label>
+			  <Button outline color="primary" size="sm">
+			  Добавить
+			  </Button>
+		      </Form>	
+		</ListGroupItem>
+        <ListGroupItem></ListGroupItem>
+      </ListGroup>
+     </div>
+	 
+	 <div>
+	  <Table >
+		<thead>
+		  <tr >
+		  <th>№ </th>
+			<th>TaskId</th>
+			<th>Title</th>
+			<th>Удалить</th>
+			<th>Сделано</th>
+		  </tr>
+		</thead>
+		<tbody>
+		{myLocalStorage.length ? (
+         myLocalStorage.map((task, item) => (
+           <tr key={item} style={{backgroundColor: task.checked ? 'aqua' : 'white'}} >
+		   <td>{item}</td>
+            <td>{task.split(":")[0]}</td>
+             <td>{task.split(":")[1]}</td>
+             <td>
+			   <Button 
+				   outline 
+				   color="primary" 
+				   size="sm"
+				   onClick={() => handleDeleteTask(item, task.split(":")[0])}
+			   >
+			   Delete</Button>
+             </td>
+			 <td>
+			 
+        <FormGroup check>
+          <Label check>
+            <Input 
+			   type="checkbox"
+               checked={task.checked}
+               //onChange={() => handleOnChange(task.id)}
+			/>{' '}
+          </Label>
+        </FormGroup>
+		</td>
+        </tr>
+		))
+		  ):(
+			<tr>
+			  <td colSpan={4}><Label><h4>Задач больше нет</h4></Label></td>
+			</tr>		
+		)}		
+		</tbody>
+            </Table>
+			</div>			
 		</div>
 	  )
 }
-
 
 
 export { AddTaskForm }
